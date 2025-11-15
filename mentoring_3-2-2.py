2f4d336e27793c02edb5a58a6e002f75a3616a74
ssttyy1 <seotaeyun0705@naver.com>
2025-11-15 16:42:00 +0900

    3주자 2번 문제(2)

---
+++ mentoring_3-2-2.py
@@ -0,0 +1,14 @@
+i = int(input()) #숫자 개수
+l = list(map(int, input().split())) #정수 입력
+
+max_num = 0 #최대값 비교를 위한 변수
+for k in range(i): #최대값 판별
+    if l[k] > max_num:
+        max_num = l[k]
+
+min_num = max_num #최소값 비교를 위한 변수
+for k in range(i): #최소값 판별
+    if l[k] < min_num + 1:
+        min_num = l[k]
+
+print(min_num, max_num)
